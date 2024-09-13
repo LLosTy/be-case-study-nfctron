d23701a59a5f9a1dd7fb0e980979f4a32c3ed9cf
LLosTy <marcojurgenjuras@gmail.com>
2024-09-13 11:19:43 +0200

    refactor(data.module.ts): removed DataService and updated exports

--- src/data/data.module.ts
+++ src/data/data.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DataService } from './data.service';
-import { DataController } from './data.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Data } from './entities/data.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Data])],
-  controllers: [DataController],
   providers: [DataService],
-  exports: [TypeOrmModule],
+  exports: [DataService, TypeOrmModule],
 })
 export class DataModule {}
